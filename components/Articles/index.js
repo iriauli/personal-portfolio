3ebfd28977b6f88786de942f166ec6e970583164
iriauli <irakliiriauli@gmail.com>
2021-12-05 02:41:36 +0400

    add new project

--- components/Articles/index.js
+++ components/Articles/index.js
@@ -3,13 +3,16 @@ import styles from "./Articles.module.css";
 const Articles = () => {
   return (
     <div className={styles.articles}>
-      <h2 className={styles.title}>Article</h2>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec
-        leo elementum, malesuada lorem finibus, facilisis ligula. Nunc
-        sollicitudin tincidunt convallis. Sed purus urna, consectetur sed
-        pretium eget, sodales ac felis.
-      </p>
+      <h2 className={styles.title}>Work Experience</h2>
+
+      <div className={styles.timeline}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec
+          leo elementum, malesuada lorem finibus, facilisis ligula. Nunc
+          sollicitudin tincidunt convallis. Sed purus urna, consectetur sed
+          pretium eget, sodales ac felis.
+        </p>
+      </div>
     </div>
   );
 };
